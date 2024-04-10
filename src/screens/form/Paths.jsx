d515c03554b2e6f254f8582569f93e6b5228f131
Harsh Khavale <harshkhavale1102@gmail.com>
2024-04-10 20:46:47 +0530

    version 0.1 mobile

--- src/screens/form/Paths.jsx
+++ src/screens/form/Paths.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
 import { learningPaths } from "../../constant";
 const Paths = () => {
   const [selectedPath, setSelectedPath] = useState();
